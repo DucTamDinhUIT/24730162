7bac07e32ff17cc2db53e58395723dde487c7524
DucTamDinhUIT <ductam.astro@gmail.com>
2024-12-24 11:17:43 +0700

    add struct

---
+++ cpp/buoi2/struct.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    struct a{
+        int a;
+        int b;
+    };
+    struct b
+    {
+        long a;
+        int a[20];
+    };
+    
+}

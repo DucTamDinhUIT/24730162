764fa27513bd70017062bf3571e37ad64894cfc8
DucTamDinhUIT <ductam.astro@gmail.com>
2025-05-21 01:57:10 +0700

    add 1

---
+++ oop/ontap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+class LopHoc
+{
+private:
+public:
+    LopHoc(/* args */);
+    ~LopHoc();
+};
+
+LopHoc::LopHoc(/* args */)
+{
+}
+
+LopHoc::~LopHoc()
+{
+}
